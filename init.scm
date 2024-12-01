8056901f25ca1826ae822142cbafa8c4ffeac759
brian.orwe <brian.orwe@gmail.com>
2024-12-01 13:22:57 +0300

    Separate wakatime

--- init.scm
+++ init.scm
@@ -1,51 +1,8 @@
 (require (prefix-in helix.static. "helix/static.scm"))
 (require (prefix-in helix. "helix/commands.scm"))
-(require (prefix-in helix.editor. "helix/editor.scm"))
-(require (prefix-in helix.misc. "helix/misc.scm"))
-(require-builtin "steel/process" as process.)
+(require (prefix-in wakatime. "./wakatime.scm"))
 
 (helix.theme "catppuccin_macchiato")
 (helix.set-option "line-number" "relative")
 
-(define wakatime-version "0.1.0")
-(define wakatime-agent "helix-wakatime")
-(define wakatime-time-key "")
-(define wakatime-exe "/home/brian/.wakatime/wakatime-cli")
-
-(define (wakatime-get-current-file)
-   (helix.editor.editor-document->path 
-     (helix.editor.editor->doc-id
-       (helix.editor.editor-focus))))
-         ; (Document-path (helix.editor.editor->get-document 
-         ;   (helix.editor.editor->doc-id 
-         ;     (helix.editor.editor-focus)))))
-    
-(define (wakatime-write cx)
-  (let* ((doc (wakatime-get-current-file))
-         (cmd (process.command wakatime-exe (list 
-                "--entity" doc
-                "--plugin" 
-                  (string-append wakatime-agent "/" wakatime-version)
-               "--write"
-                ))))
-    
-    (if doc (process.spawn-process cmd) ) ))
-
-(define (wakatime-listen-inserts )
-  (register-hook! "post-insert-char" "wakatime-write"))
-(wakatime-listen-inserts)
-
-(define (wakatime-heart-beat)
-  (let ((doc (wakatime-get-current-file)))
-
-    (if doc (process.spawn-process (process.command wakatime-exe (list 
-                "--entity" doc
-                "--plugin" 
-                (string-append wakatime-agent "/" wakatime-version))))
-        #f))
-  (run-wakatime))
-
-(define (run-wakatime)
-  (helix.misc.enqueue-thread-local-callback-with-delay 1000 wakatime-heart-beat ))
-
-(run-wakatime)
+(wakatime.run-wakatime)
