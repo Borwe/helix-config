cf37c938be88122132965b323b13342c616f8e95
brian.orwe <brian.orwe@gmail.com>
2025-05-29 15:56:35 +0300

    Update config

--- init.scm
+++ init.scm
@@ -1,8 +1,10 @@
 (require (prefix-in helix.static. "helix/static.scm"))
 (require (prefix-in helix. "helix/commands.scm"))
 (require (prefix-in wakatime. "cogs/wakatime.scm"))
+(require (prefix-in helix.conf. "helix/configuration.scm"))
 
 (helix.theme "catppuccin_macchiato")
 (helix.set-option "line-number" "relative")
+(helix.set-option "cursorline" "true")
 
 (wakatime.run-wakatime)
